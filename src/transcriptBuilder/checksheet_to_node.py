907d324ecbaec49b89e41d632bf6f9b1645dfd62
Redaxxed <118015417+Redaxxed@users.noreply.github.com>
2023-10-03 11:13:12 -0400

    Notes on prototype design

---
+++ src/transcriptBuilder/checksheet_to_node.py
@@ -0,0 +1,3 @@
+# List for each node (type of course in checksheet)
+# Required classes are string
+# Courses with options will be a list within the primary list
---
+++ src/transcriptBuilder/database_interface.py
@@ -0,0 +1,3 @@
+# Waiting to see what database we are using
+# If azure need to make a new file
+# If acad may be able to use prototype
